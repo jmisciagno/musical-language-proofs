91ed575092f4241176fe47de42ea52ca46ea18d7
jmisciagno <misciag2@uwm.edu>
2018-02-28 20:51:31 +0000

    Update proofs.v

--- proofs.v
+++ proofs.v
@@ -1,84 +1,102 @@
-Variables Statement Concept : Type.
-Variable eval : Statement -> Concept -> Prop.
-Definition Language : Type := Statement -> Prop.
+Section Sets.
+  Definition intersection {A} : (A -> Prop) -> (A -> Prop) -> (A -> Prop) :=
+    fun r s x => r x /\ s x.
+  
+  Definition union {A} : (A -> Prop) -> (A -> Prop) -> (A -> Prop) :=
+    fun r s x => r x \/ s x.
 
-Definition subset {A} : (A -> Prop) -> (A -> Prop) -> Prop :=
-  fun r s => forall x, r x -> s x.
+  Definition superset {A} : (A -> Prop) -> (A -> Prop) -> Prop :=
+    fun r s => forall x, s x -> r x.
+  
+  Definition equal {A} : (A -> Prop) -> (A -> Prop) -> Prop :=
+    fun r s => superset r s /\ superset s r.
+End Sets.
 
-Definition superset {A} : (A -> Prop) -> (A -> Prop) -> Prop :=
-  fun r s => forall x, s x -> r x.
+Section Raise.
+  Definition raise {A B} : (A -> B -> Prop) -> (A -> Prop) -> (B -> Prop) -> Prop :=
+    fun r s t => forall (a : A) (b : B), s a -> r a b -> t b.
 
-Definition equal {A} : (A -> Prop) -> (A -> Prop) -> Prop :=
-  fun r s => superset r s /\ superset s r.
+  Theorem P1 {A B} :
+    forall (r : A -> B -> Prop) (s t : A -> Prop),
+      superset (raise r (union s t)) (intersection (raise r s) (raise r t)).
+    intros.
+    compute.
+    intros.
+    inversion H.
+    inversion H0.
+    apply (H2 a b H4 H1).
+    apply (H3 a b H4 H1).
+  Defined.
+  
+  Theorem P2 {A B} :
+    forall (r : A -> B -> Prop) (s t : A -> Prop),
+      superset (raise r (intersection s t)) (union (raise r s) (raise r t)).
+    intros.
+    compute.
+    intros.
+    inversion H0.
+    inversion H.
+    apply (H4 a b H2 H1).
+    apply (H4 a b H3 H1).
+  Defined.
+  
+  Theorem P3 {A B} :
+    forall (r : A -> B -> Prop) (s t : A -> Prop),
+      superset (intersection (raise r s) (raise r t)) (raise r (union s t)).
+    intros.
+    compute.
+    intros.
+    split.
+    intros.
+    apply (H a b (or_introl H0) H1).
+    intros.
+    apply (H a b (or_intror H0) H1).
+  Defined.
+  
+  Theorem P4 {A B} :
+    forall (r : A -> B -> Prop) (s t : A -> Prop),
+      superset (union (raise r s) (raise r t)) (raise r (union s t)).
+    intros.
+    compute.
+    intros.
+    left.
+    intros.
+    apply (H a b (or_introl H0) H1).
+  Defined.
 
-Definition intersection {A} : (A -> Prop) -> (A -> Prop) -> (A -> Prop) :=
-  fun r s x => r x /\ s x.
+  Theorem P4' {A B} :
+    forall (r : A -> B -> Prop) (s t : A -> Prop),
+      superset (union (raise r s) (raise r t)) (raise r (union s t)).
+    intros.
+    compute.
+    intros.
+    right.
+    intros.
+    apply (H a b (or_intror H0) H1).
+  Defined.
 
-Definition union {A} : (A -> Prop) -> (A -> Prop) -> (A -> Prop) :=
-  fun r s x => r x \/ s x.
+  Theorem P5 {A B} :
+    forall (r : A -> B -> Prop) (s t: A -> Prop),
+      equal (raise r (union s t)) (intersection (raise r s) (raise r t)).
+    unfold equal.
+    split.
+    apply (P1 r s t).
+    apply (P3 r s t).
+  Defined.
+  
+  Definition P6_Type A B :=
+    forall (r : A -> B -> Prop) (s t : A -> Prop),
+      superset (union (raise r s) (raise r t)) (raise r (intersection s t)).
+  
+  Theorem P7 {A B} :
+    forall (r : A -> B -> Prop) (s t : A -> Prop),
+      (P6_Type A B) -> equal (raise r (intersection s t)) (union (raise r s) (raise r t)).
+    unfold P6_Type.
+    unfold equal.  
+    intros.
+    split.
+    apply (P2 r s t).
+    apply (H r s t).
+  Defined.
+End Raise.  
 
-Definition interpret : Language -> (Concept -> Prop) -> Prop :=
-  fun (L : Statement -> Prop) (X : Concept -> Prop) =>
-    forall (s : Statement),
-      L s -> subset (eval s) X.
-
-Theorem P1 :
-  forall (L1 L2 : Language),
-    superset (interpret (union L1 L2)) (intersection (interpret L1) (interpret L2)).
-  intros.
-  compute.
-  intros.
-  inversion H.
-  inversion H0.
-  apply (H2 s H4).
-  apply H1.
-  apply (H3 s H4).
-  apply H1.
-Defined.
-
-Theorem P2 : forall (L1 L2 : Language),
-    superset (interpret (intersection L1 L2)) (union (interpret L1) (interpret L2)).
-  intros.
-  compute.
-  intros.
-  inversion H0.
-  inversion H.
-  apply (H4 s H2 x0 H1).
-  apply (H4 s H3 x0 H1).
-Defined.
-
-Theorem P3 :
-  forall (L1 L2 : Language),
-    superset (intersection (interpret L1) (interpret L2)) (interpret (union L1 L2)).
-  intros.
-  compute.
-  intros.
-  split.
-  intros.
-  apply (H s (or_introl H0) x0 H1).
-  intros.
-  apply (H s (or_intror H0) x0 H1).
-Defined.
-
-Theorem P4 :
-  forall (L1 L2 : Language),
-    superset (union (interpret L1) (interpret L2)) (interpret (union L1 L2)).
-  intros.
-  compute.
-  intros.
-  left.
-  intros.
-  apply (H s).
-  left.
-  apply H0.
-  apply H1.
-Defined.
-
-Theorem P5 :
-  forall (L1 L2 : Language),
-    equal (interpret (union L1 L2)) (intersection (interpret L1) (interpret L2)).
-  unfold equal.
-  split.
-  apply (P1 L1 L2).
-  apply (P3 L1 L2).
-Defined.
