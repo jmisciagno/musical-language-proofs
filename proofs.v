2eab2e931c947a236452b162169c3ee4312d0485
jmisciagno <misciag2@uwm.edu>
2018-03-10 22:49:34 -0600

    Update proofs.v

--- proofs.v
+++ proofs.v
@@ -8,76 +8,74 @@ Section Sets.
   Definition subset {A} : (A -> Prop) -> (A -> Prop) -> Prop :=
     fun r s => forall x, r x -> s x.
 
-  Definition superset {A} : (A -> Prop) -> (A -> Prop) -> Prop :=
-    fun r s => forall x, s x -> r x.
-  
   Definition equal {A} : (A -> Prop) -> (A -> Prop) -> Prop :=
-    fun r s => superset r s /\ superset s r.
+    fun r s => subset r s /\ subset s r.
+
 End Sets.
 
-Section A.
-  Variables Statement Concept : Type.
-  Variable eval : Statement -> Concept -> Prop.
+Definition raise {A B} : (A -> B -> Prop) -> (A -> Prop) -> B -> Prop :=
+  fun r s b => forall a, s a -> r a b.
 
-  Definition a : (Statement -> Prop) -> (Concept -> Prop) -> Prop :=
-    fun s t => forall (x : Statement), s x -> superset (eval x) t.
+Theorem P1 {A B} :
+  forall (r : A -> B -> Prop) (s t : A -> Prop),
+    subset (intersection (raise r s) (raise r t)) (raise r (union s t)) .
+  compute.
+  intros.
+  inversion H.
+  inversion H0.
+  apply (H1 a H3).
+  apply (H2 a H3).
+Defined.
 
-  Theorem P1 :
-    forall (s t : Statement -> Prop),
-      superset (a (union s t)) (intersection (a s) (a t)).
-      firstorder.
-    Defined.
+Theorem P2 {A B} :
+  forall (r : A -> B -> Prop) (s t : A -> Prop),
+    subset (raise r (union s t)) (intersection (raise r s) (raise r t)).
+  compute.
+  intros.
+  split.
+  intros.
+  apply (H a (or_introl H0)).
+  intros.
+  apply (H a (or_intror H0)).
+Defined.
 
-  Theorem P2 :
-    forall (s t : Statement -> Prop),
-      superset (a (intersection s t)) (union (a s) (a t)).
-    firstorder.
-  Defined.
+Theorem P3 {A B} :
+  forall (r : A -> B -> Prop) (s t : A -> Prop),
+    subset (union (raise r s) (raise r t)) (raise r (intersection s t)) .
+  compute.
+  intros.
+  inversion H0.
+  inversion H.
+  apply (H3 a H1).
+  apply (H3 a H2).
+Defined.
 
-  Theorem P3 :
-    forall (s t : Statement -> Prop),
-      superset (intersection (a s) (a t)) (a (union s t)).
-    firstorder.
-  Defined.
+Theorem P4 {A B} :
+  forall (r : A -> B -> Prop) (s t : A -> Prop),
+    subset (raise r (union s t)) (union (raise r s) (raise r t)).
+  compute.
+  intros.
+  left.
+  intros.
+  apply (H a (or_introl H0)).
+Defined.
+
+Theorem P5 {A B} :
+  forall (r : A -> B -> Prop) (s t : A -> Prop),
+    subset (intersection (raise r s) (raise r t)) (raise r (intersection s t)) .
+  compute.
+  intros.
+  inversion H0.
+  inversion H.
+  apply (H3 a H1).
+Defined.
   
-  Theorem P4 :
-    forall (s t : Statement -> Prop),
-      superset (union (a s) (a t)) (a (union s t)).
-    firstorder.
-  Defined.
-
-  Theorem P5 :
-    forall (s t : Statement -> Prop),
-    equal (a (union s t)) (intersection (a s) (a t)).
-    firstorder.
-  Defined.
-End A.
-
-Section Raise.
-  Definition raise {A B} : (A -> B -> Prop) -> (A -> Prop) -> (B -> Prop) -> Prop :=
-    fun r s t => forall (x : A), s x -> superset (r x) t.
-
-    Theorem P1' {A B} :
-    forall (r : A -> B -> Prop) (s t : A -> Prop),
-      superset (raise r (union s t)) (intersection (raise r s) (raise r t)).
-      firstorder.
-    Defined.
-
-    Theorem P2' {A B} :
-      forall (r : A -> B -> Prop) (s t : A -> Prop),
-        superset (raise r (intersection s t)) (union (raise r s) (raise r t)).
-      firstorder.
-    Defined.
-
-    Theorem P3' {A B} :
-      forall (r : A -> B -> Prop) (s t : A -> Prop),
-        superset (intersection (raise r s) (raise r t)) (raise r (union s t)).
-      firstorder.
-    Defined.
-
-    Theorem P4' {A B} :
-      forall (r : A -> B -> Prop) (s t : A -> Prop),
-        superset (union (raise r s) (raise r t)) (raise r (union s t)).
-      firstorder.
-    Defined.
-End Raise.
+Theorem P6 {A B} :
+  forall (r : A -> B -> Prop) (s t : A -> Prop),
+    equal (intersection (raise r s) (raise r t)) (raise r (union s t)) .
+  unfold equal.
+  intros.
+  split.
+  apply (P1 r s t).
+  apply (P2 r s t).
+Defined.
